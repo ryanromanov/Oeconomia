e413a247376c6cae36961f335c056a2c60f693c8
ryanromanosky <ryanromanov@github.com>
2019-06-08 19:53:10 -0400

    OEController Commits - Added method to grab valid input for principle amount
    Oeconomia Commits
    - added class method so program would run

--- src/OEController.java
+++ src/OEController.java
@@ -1,4 +1,6 @@
-class OEController implements Scriver {
+import java.io.IOException;
+
+class OEController implements Scriver, Leger {
     private OEModel Model;
 
     OEController(OEModel pModel) {
@@ -17,11 +19,42 @@ class OEController implements Scriver {
 
 
     public void printMenu() {
+        print("************************************");
+        print("************************************");
+        print("*************Oeconomia**************");
+        getValidPValue();
 
+
+
+    }
+
+    private void getValidPValue() {
+        String pTemp;
+        print("Enter principle amount: ");
+        print("p = ");
+        pTemp = read();
+        while (!pTemp.matches("[+]?[0-9]*\\.?[0-9]+")) {
+            print("**Invalid number entered. Please enter a valid princple amount.");
+            print("p = ");
+            pTemp = read();
+        }
     }
 
     @Override
     public void print(Object line) {
         System.out.println(line.toString());
+    }
+
+
+    @Override
+    public String read() {
+        String InputString = "";
+        try {
+            InputString = bufferedInput.readLine();
+        } catch (IOException e) {
+            print("**Error in read() method in OEController's Leger");
+            e.printStackTrace();
+        }
+        return InputString;
     }
 }
--- src/Oeconomia.java
+++ src/Oeconomia.java
@@ -5,7 +5,7 @@ public class Oeconomia implements Scriver {
     public static void main(String[] args) {
         OEModel model = new OEModel();
         OEController controller = new OEController(model);
-
+        controller.runProgram();
 
     }
 
